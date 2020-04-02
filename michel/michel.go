e033646d67af9def0050bf67a689e1816c6d5b22
Jeffry Hesse <jhesse@sonatype.com>
2020-04-01 17:26:12 -0800

    MASTER MASTER PULLING THE STRINGS

--- michel/michel.go
+++ michel/michel.go
@@ -1,15 +1,29 @@
 package michel
 
 import (
-	"fmt"
+	"io"
 )
+
+type Thing interface {
+	DoAnotherThing()
+}
+
+type ThingImpl struct {
+}
+
+func (t ThingImpl) Read(thing []byte) (int, error) {
+	return 1, nil
+}
 
 var michel string
 
 func init() {
 	michel = "Michel"
+
+	thing := ThingImpl{}
+	ThingConsumer(thing)
 }
 
-func DoAnotherThing() {
-	fmt.Println("Michel")
+func ThingConsumer(thing io.Reader) {
+	thing.Read([]byte("String"))
 }
